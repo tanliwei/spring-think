7ecfdceb611c98128148e66d2502a73990d05efd
Tanliwei <236328172@qq.com>
2018-10-08 15:23:30 +0800

    PropertySourceHelper

--- src/main/java/com/newnoa/framework/annotation/x/DoubleClickAspect.java
+++ src/main/java/com/newnoa/framework/annotation/x/DoubleClickAspect.java
@@ -8,12 +8,16 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.util.DigestUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 重复点击
+ *
  * @create 2018/7/23
  */
 @Aspect
@@ -23,21 +27,23 @@ public class DoubleClickAspect {
     private Map redisTemplate = new HashMap();
 
     @Pointcut("@annotation(com.newnoa.framework.annotation.x.DoubleClick)")
-    public void doubleClickPointCut(){}
+    public void doubleClickPointCut() {
+    }
 
     @Before("doubleClickPointCut() && @annotation(doubleClick)")
-    public void doubleClickCheck(JoinPoint point, DoubleClick doubleClick){
-
+    public void doubleClickCheck(JoinPoint point, DoubleClick doubleClick) {
+        String authorization = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("Authorization");
         Signature signature = point.getSignature();
-        String fullMethodName = signature.getDeclaringTypeName() +"."+ signature.getName();
+        String fullMethodName = signature.getDeclaringTypeName() + "." + signature.getName();
         int timeout = doubleClick.timeout();
         Object[] args = point.getArgs();
-        String doubleClickKey;
+        StringBuilder doubleClickKey = new StringBuilder();
         if (args == null || args.length == 0) {
-            doubleClickKey = fullMethodName;
+            doubleClickKey.append(fullMethodName);
         } else {
-            doubleClickKey = fullMethodName+ DigestUtils.md5DigestAsHex(JSON.toJSONBytes(args));
+            doubleClickKey.append(fullMethodName).append(DigestUtils.md5DigestAsHex(JSON.toJSONBytes(args)));
         }
+        doubleClickKey.append(authorization);
         boolean isDoubleClick = redisTemplate.get(doubleClickKey) != null;
         if (isDoubleClick) {
             throw new RuntimeException("请勿重复点击");
--- src/main/java/com/newnoa/util/PropertySourceHelper.java
+++ src/main/java/com/newnoa/util/PropertySourceHelper.java
@@ -1,8 +1,73 @@
 package com.newnoa.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+
 /**
- * @author liwei.tan@baozun.com
- * @Date 2018/8/31 15:32
+ *
+ * @author liwei.tan
+ * @Date 2018/8/31 14:46
  */
+@Slf4j
 public class PropertySourceHelper {
+    //classpath对应resources目录, file对应项目根路径
+    private static final String DEFAULT_SEARCH_LOCATIONS = "classpath:/,classpath:/config/,file:./,file:./config/";
+    private static final String DEFAULT_SEARCH_FILENAME = "application.properties,application.xml,application.yml,application.yaml";
+    public static final String SPRING_PROFILES_ACTIVE = "spring.profiles.active";
+
+
+    /**
+     * 读取配置文件的spring.profiles.active属性
+     * @return
+     */
+    public String getSpringProfileActive() {
+        return getProperties().getProperty(SPRING_PROFILES_ACTIVE);
+    }
+
+    public Properties getProperties() {
+        Properties properties = new Properties();
+        DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource;
+        String[] locations = StringUtils.commaDelimitedListToStringArray(DEFAULT_SEARCH_LOCATIONS);
+        String[] filenames = StringUtils.commaDelimitedListToStringArray(DEFAULT_SEARCH_FILENAME);
+        //组合路径和文件名 检查本地是否有配置文件
+        //TODO spring.config.name 自定义了资源配置路径的情况
+        for (String location : locations) {
+            for (String filename : filenames) {
+                resource = resourceLoader.getResource(location + filename);
+                if (resource != null && resource.exists()) {
+                    try {
+                        loadResource(properties, resource, filename);
+                        return properties;
+                    } catch (IOException e) {
+                        log.error("getURI IOException", e);
+                    }
+                }
+            }
+        }
+        return properties;
+    }
+
+    private void loadResource(Properties properties, Resource resource, String filename) throws IOException {
+        try {
+            if ("application.properties".equals(filename) || "application.xml".equals(filename)) {
+                properties.load(new FileInputStream(resource.getFile()));
+            }
+            if ("application.yml".equals(filename) || "application.yaml".equals(filename)) {
+                YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+                yaml.setResources(resource);//File引入
+                properties.putAll(yaml.getObject());
+            }
+        } catch (Exception e) {
+            log.error("加载配置文件异常", e);
+        }
+    }
 }
